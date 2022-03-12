a7f4e8010bdbb0b516dd99d4bae2951a60e7bd16
Abdullah <73560425+zer-far@users.noreply.github.com>
2022-03-12 15:19:43 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+sudo apt autoclean
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+sudo apt install ufw -y
+sudo ufw start
+sudo ufw enable
+sudo ufw deny all
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 25565
+sudo docker-compose up -d
+sudo mv data /
