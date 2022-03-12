9c6990c2e2aeb07b3d3c000d9996ccd5c84a3d91
Abdullah <73560425+zer-far@users.noreply.github.com>
2022-03-12 15:23:47 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+# Apt
 sudo apt update
 sudo apt upgrade
 sudo apt autoremove
@@ -5,11 +6,15 @@ sudo apt autoclean
 sudo apt install docker.io -y
 sudo apt install docker-compose -y
 sudo apt install ufw -y
+
+# Firewall
 sudo ufw start
 sudo ufw enable
 sudo ufw deny all
 sudo ufw allow 80
 sudo ufw allow 443
 sudo ufw allow 25565
+
+# Docker
 sudo docker-compose up -d
 sudo mv data /
